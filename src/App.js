cf16fabaf472e9b1d0d2c9776cab2db3728b435b
Aoife <aoifeebradley@gmail.com>
2021-05-13 00:29:03 +0200

    Add city card component and initial styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CityCard from "./cityCard.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="h-screen bg-purple-400 flex flex-col">
+      <header className="flex items-center bg-white h-16">
+        <input
+          type="search"
+          className="ml-4 py-2 px-4 rounded-md border border-purple-500 outline-none"
+        />
       </header>
+      <section className="flex-grow w-full flex flex-col justify-center items-center">
+        <CityCard></CityCard>
+      </section>
     </div>
   );
 }
---
+++ src/cityCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CityCard() {
+  return (
+    <div className="rounded-2xl bg-white w-5/6 flex-grow my-10">
+      <div></div>
+    </div>
+  );
+}
